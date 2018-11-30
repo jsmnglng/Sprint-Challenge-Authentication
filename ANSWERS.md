20dc369c2ae0dcf17922b79ad01a960898f36f74
jsmnglng <jsmn.glng@gmail.com>
2018-11-30 08:16:25 -0800

    Answers.md and project setup

--- ANSWERS.md
+++ ANSWERS.md
@@ -2,8 +2,17 @@
 
 1. What is the purpose of using _sessions_?
 
+   - It’s a way to keep state in the server level between requests
+
 2. What does bcrypt do to help us store passwords in a secure manner.
+
+   - It uses a function to hash away your password multiple times to add more complex security.
 
 3. What does bcrypt do to slow down attackers?
 
+   - Bcrypt iteratively re-hashing the hashed password so it adds more time to slow down attackers
+
 4. What are the three parts of the JSON Web Token?
+   - header
+   - payload
+   - signature
--- config/routes.js
+++ config/routes.js
@@ -1,12 +1,17 @@
-const axios = require('axios');
+const axios = require("axios");
 
-const { authenticate } = require('./middlewares');
+const { authenticate } = require("./middlewares");
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
+  server.get("/", testAPI);
 };
+
+function testAPI(_, res) {
+  res.send("API is live");
+}
 
 function register(req, res) {
   // implement user registration
@@ -19,12 +24,12 @@ function login(req, res) {
 function getJokes(req, res) {
   axios
     .get(
-      'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten'
+      "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten"
     )
     .then(response => {
       res.status(200).json(response.data);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
