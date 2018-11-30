f020c292d753364e1962d9b5f6b1eeeef49c7b49
jsmnglng <jsmn.glng@gmail.com>
2018-11-30 08:55:23 -0800

    Navigation and routing initial setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { NavLink, Route, Switch } from "react-router-dom";
+
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <nav>
+            <NavLink exact to="/" activeStyle={{ textDecoration: "underline" }}>
+              Home
+            </NavLink>
+            &nbsp;|&nbsp;
+            <NavLink
+              to="/register"
+              activeStyle={{ textDecoration: "underline" }}
+            >
+              Register
+            </NavLink>
+            &nbsp;|&nbsp;
+            <NavLink to="/login" activeStyle={{ textDecoration: "underline" }}>
+              Login
+            </NavLink>
+          </nav>
         </header>
+        <section>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+          </Switch>
+        </section>
       </div>
     );
   }
